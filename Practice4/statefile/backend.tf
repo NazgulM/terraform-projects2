7f0d7b25d2287364c3c08fa1d3a979b37802eb39
NazgulM <maniyasova.n@gmail.com>
2023-04-29 18:26:54 +0000

    delete large

--- Practice4/statefile/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "nazgul-bucket-practice4"
-    key    = "path/to/my/key"
-    region = "us-east-1"
-    dynamodb_table = "lock-state"
-  }
-}
--- Practice4/statefile/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = file("~/.ssh/id_rsa.pub")
-  tags = {
-    "team" = "devops"
-  }
-}
